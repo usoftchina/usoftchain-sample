6ebc08882f98940822569dc059be7673a5c095ae
yingp <yingp@usoftchina.com>
2018-09-06 12:13:43 +0800

    example

--- e2e_cli/network_setup.sh
+++ e2e_cli/network_setup.sh
@@ -147,23 +147,21 @@ EOF
 function stopPeer() {
     for key in ${!PEERS[@]}
     do
-      file="docker-compose-${PEERS[$key]}.yaml"
-      ssh -T $key <<EOF
-      cd go/src/github.com/usoftchina/usoftchain-sample/e2e_cli
-      docker-compose -f $file down
+      file="go/src/github.com/usoftchina/usoftchain-sample/e2e_cli/docker-compose-${PEERS[$key]}.yaml"
+      ssh $key "docker-compose -f $file down"
       # clear unwanted containers
-      CONTAINER_IDS=$(docker ps -a | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $1}')
+      CONTAINER_IDS=$(ssh $key "docker ps -a | grep \"dev\|none\|test-vp\|peer[0-9]-\" | awk '{print $1}'")
       if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
           echo "---- No containers available for deletion ----"
       else
-          docker rm -f $CONTAINER_IDS
+          ssh $key "docker rm -f $CONTAINER_IDS"
       fi
       # remove unwanted images
-      DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+      DOCKER_IMAGE_IDS=$(ssh $key "docker images | grep \"dev\|none\|test-vp\|peer[0-9]-\" | awk '{print $3}'")
       if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
           echo "---- No images available for deletion ----"
       else
-          docker rmi -f $DOCKER_IMAGE_IDS
+          ssh $key "docker rmi -f $DOCKER_IMAGE_IDS"
       fi
 
       exit

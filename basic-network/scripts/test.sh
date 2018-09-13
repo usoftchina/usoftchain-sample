79b80cd632ddf84ab8372cf9c0a7e99b06004756
yingp <yingp@usoftchina.com>
2018-09-13 18:55:25 +0800

    basic network

--- basic-network/scripts/test.sh
+++ basic-network/scripts/test.sh
@@ -14,3 +14,6 @@ peer channel join -b mychannel.block
 peer chaincode install -n stockcontract -v 1.0 -p github.com/usoftchina/usoftchain-sample/chaincode/stock
 # instantiate chaincode
 peer chaincode instantiate -o orderer.usoftchain.com:7050 -C mychannel -n stockcontract -v 1.0 -c '{"Args":["init"]}' -P "OR ('XinningMSP.member')"
+sleep 2
+# invoke chaincode
+peer chaincode invoke -o orderer.usoftchain.com:7050 -C mychannel -n stockcontract -c '{"Args":["createWarehouse","新宁", "新宁仓", "广东省东莞市长安镇乌沙第六工业区海滨路23号"]}'
